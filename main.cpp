c464beaed803dd17b55357686013af8d2c202406
antonov1lya <a222i@ya.ru>
2022-03-10 01:02:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
             {'g',{'f'}},
             {'h',{'g','d'}}
     });
-    std::cout << a.IsUndirected() << a.EdgesNumber() << a.PrintSets() << a.Distance('a','h');
+    std::cout << a.IsUndirected() << a.EdgesNumber() << a.PrintSets() << a.Distance('a','g');
     std::cout << a.VerticesNumber() << a.CheckLoops() << a.PrintAdjacencyMatrix() << a.PrintIncidenceMatrix();
     std::cout << a.PrintAdjacencyList() << a.PrintVertexDegree('b');
     std::cout << a.StrongConnectivityComponents() << a.WeakConnectivityComponents();
