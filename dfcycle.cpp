572ab9c3b1e80b0384f0b3ad30f166f7532deccc
Laitielly <78595002+Laitielly@users.noreply.github.com>
2022-03-13 17:00:04 +0300

    correct dfcycle

--- dfcycle.cpp
+++ dfcycle.cpp
@@ -1,12 +1,12 @@
 #include"clique.h"
 
-CyclicityResult GreedMaxClique::dfs(int vertex, bool *used, int parent = -1)
+CyclicityResult GreedMaxClique::dfs(int vertex, bool used[], int parent = -1)
 {
     used[vertex] = true;
     for (auto neighbor : m_adjacency_list[vertex])
     {
         if (!used[neighbor])
-            dfs(neighbor, used,vertex);
+            return dfs(neighbor, used,vertex);
         else if(neighbor != parent)
         {
             return CyclicityResult::HasCycle;
