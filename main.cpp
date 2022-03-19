4e26469543fe8d76ba28d9190d26bab78137aed8
antonov1lya <a222i@ya.ru>
2022-03-19 20:39:33 +0300

    bug test

--- main.cpp
+++ main.cpp
@@ -33,32 +33,29 @@ int main() {
 //    std::cout << a.IndependentSetSize(4);
 //    std::cout << a.IndependentSetSize(9);
 
-    const Graph<Point> a(MatrixType::ADJACENCY, {
-            {1, 0, 0, 1, 0, 1},
-            {0, 0, 0, 1, 1, 1},
-            {0, 1, 1, 0, 1, 0},
-            {1, 1, 0, 1, 1, 0},
-            {0, 0, 1, 1, 0, 1},
-            {0, 1, 1, 0, 1, 0}
+    const Graph<char> a(MatrixType::ADJACENCY, {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 1, 0}
     },
-                         {Point(1,2),Point(2,3),Point(3,4),Point(4,5),Point(5,6),Point(6,7)});
-    std::cout << a.IsUndirected() << a.EdgesNumber() << a.PrintSets() << a.Distance(Point(1,1),Point(4,5));
-    std::cout << a.VerticesNumber() << a.CheckLoops() << a.PrintAdjacencyMatrix() << a.PrintIncidenceMatrix();
-    std::cout << a.PrintAdjacencyList() << a.PrintVertexDegree(Point(1,1));
-    std::cout << a.StrongConnectivityComponents() << a.WeakConnectivityComponents();
-    std::cout << a.MaxClique() << a.SizeClique(3);
-    std::cout << a.FindCycles() << a.FindCyclesSize(3);
-    std::cout << a.FindCentralVertices();
-    std::cout << a.FindDiameter();
-    std::cout << a.FindRadius();
-    std::cout << a.Eccentricity(Point(3,4));
-    std::cout << a.IsTree();
-    std::cout << a.MaxIndependentSet();
-    std::cout << a.IndependentSetSize(0);
-    std::cout << a.IndependentSetSize(1);
-    std::cout << a.IndependentSetSize(2);
-    std::cout << a.IndependentSetSize(3);
-    std::cout << a.IndependentSetSize(4);
-    std::cout << a.IndependentSetSize(9);
+                         {'a','b','c'});
+//    std::cout << a.IsUndirected() << a.EdgesNumber() << a.PrintSets() << a.Distance(Point(1,1),Point(3,4));
+//    std::cout << a.VerticesNumber() << a.CheckLoops() << a.PrintAdjacencyMatrix() << a.PrintIncidenceMatrix();
+//    std::cout << a.PrintAdjacencyList() << a.PrintVertexDegree(Point(1,1));
+//    std::cout << a.StrongConnectivityComponents() << a.WeakConnectivityComponents();
+    std::cout << a.MaxClique() << a.SizeClique(2);
+//    std::cout << a.FindCycles() << a.FindCyclesSize(3);
+//    std::cout << a.FindCentralVertices();
+//    std::cout << a.FindDiameter();
+//    std::cout << a.FindRadius();
+//    std::cout << a.Eccentricity(Point(1,2));
+//    std::cout << a.IsTree();
+//    std::cout << a.MaxIndependentSet();
+//    std::cout << a.IndependentSetSize(0);
+//    std::cout << a.IndependentSetSize(1);
+//    std::cout << a.IndependentSetSize(2);
+//    std::cout << a.IndependentSetSize(3);
+//    std::cout << a.IndependentSetSize(4);
+//    std::cout << a.IndependentSetSize(9);
     return 0;
 }
