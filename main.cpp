70ec9e8b2f1ec30ac47ec10601d7658ad1cd9c65
antonov1lya <a222i@ya.ru>
2022-03-19 20:02:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -33,8 +33,16 @@ int main() {
 //    std::cout << a.IndependentSetSize(4);
 //    std::cout << a.IndependentSetSize(9);
 
-    const Graph<Point> a({Point(1,1),Point(1,2),Point(1,3)},{{Point(1,1),Point(1,3)}});
-    std::cout << a.IsUndirected() << a.EdgesNumber() << a.PrintSets() << a.Distance(Point(1,1),Point(1,3));
+    const Graph<Point> a(MatrixType::ADJACENCY, {
+            {1, 0, 0, 1, 0, 1},
+            {0, 0, 0, 1, 1, 1},
+            {0, 1, 1, 0, 1, 0},
+            {1, 1, 0, 1, 1, 0},
+            {0, 0, 1, 1, 0, 1},
+            {0, 1, 1, 0, 1, 0}
+    },
+                         {Point(1,2),Point(2,3),Point(3,4),Point(4,5),Point(5,6),Point(6,7)});
+    std::cout << a.IsUndirected() << a.EdgesNumber() << a.PrintSets() << a.Distance(Point(1,1),Point(4,5));
     std::cout << a.VerticesNumber() << a.CheckLoops() << a.PrintAdjacencyMatrix() << a.PrintIncidenceMatrix();
     std::cout << a.PrintAdjacencyList() << a.PrintVertexDegree(Point(1,1));
     std::cout << a.StrongConnectivityComponents() << a.WeakConnectivityComponents();
@@ -43,7 +51,7 @@ int main() {
     std::cout << a.FindCentralVertices();
     std::cout << a.FindDiameter();
     std::cout << a.FindRadius();
-    std::cout << a.Eccentricity(Point(1,1));
+    std::cout << a.Eccentricity(Point(3,4));
     std::cout << a.IsTree();
     std::cout << a.MaxIndependentSet();
     std::cout << a.IndependentSetSize(0);
