5e8d4503e8e077ccff96b586564b7e1888c7f568
antonov1lya <a222i@ya.ru>
2022-03-22 17:11:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,34 +6,34 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
 
     const Graph<std::string> a(MatrixType::ADJACENCY, {
-                                {0, 1, 0, 0},
-                                {1, 1, 1, 1},
-                                {1, 0, 0, 0},
-                                {0, 1, 0, 1}
+                                {0, 0, 0, 0},
+                                {0, 0, 0, 0},
+                                {0, 0, 0, 0},
+                                {0, 0, 0, 0}
     },
                          {"a","b","c","d"}
     );
 
-    std::cout << a.IsUndirected() << a.EdgesNumber() << a.PrintSets();
-    std::cout << a.VerticesNumber() << a.CheckLoops() << a.PrintAdjacencyMatrix() << a.PrintIncidenceMatrix();
-    std::cout << a.PrintAdjacencyList();
-    std::cout << a.StrongConnectivityComponents() << a.WeakConnectivityComponents();
-    std::cout << a.MaxClique() << a.SizeClique(2);
-    std::cout << a.FindCycles() << a.FindCyclesSize(3);
-    std::cout << a.FindCentralVertices();
-    std::cout << a.FindDiameter();
-    std::cout << a.FindRadius();
-    std::cout << a.IsTree();
-    std::cout << a.MaxIndependentSet();
-    std::cout << a.IndependentSetSize(0);
-    std::cout << a.IndependentSetSize(1);
-    std::cout << a.IndependentSetSize(2);
-    std::cout << a.IndependentSetSize(3);
-    std::cout << a.IndependentSetSize(4);
-    std::cout << a.IndependentSetSize(9);
+//    std::cout << a.IsUndirected() << a.EdgesNumber() << a.PrintSets();
+//    std::cout << a.VerticesNumber() << a.CheckLoops() << a.PrintAdjacencyMatrix() << a.PrintIncidenceMatrix();
+//    std::cout << a.PrintAdjacencyList();
+//    std::cout << a.StrongConnectivityComponents() << a.WeakConnectivityComponents();
+//    std::cout << a.MaxClique() << a.SizeClique(2);
+//    std::cout << a.FindCycles() << a.FindCyclesSize(3);
+//    std::cout << a.FindCentralVertices();
+//    std::cout << a.FindDiameter();
+//    std::cout << a.FindRadius();
+//    std::cout << a.IsTree();
+//    std::cout << a.MaxIndependentSet();
+//    std::cout << a.IndependentSetSize(0);
+//    std::cout << a.IndependentSetSize(1);
+//    std::cout << a.IndependentSetSize(2);
+//    std::cout << a.IndependentSetSize(3);
+//    std::cout << a.IndependentSetSize(4);
+//    std::cout << a.IndependentSetSize(9);
     std::cout << a.IsBipartite() << a.OptimalColoring();
-    std::cout << a.IsEulerGraph();
-    std::cout << a.IsHamiltonGraph();
+//    std::cout << a.IsEulerGraph();
+//    std::cout << a.IsHamiltonGraph();
 
     return 0;
 }
